3ffd8adba3b627b330167d9b0fbc4862c5d95833
becc-mu <becc.mulugeta@gmail.com>
2019-11-25 16:39:21 +0000

    added useEffect solution

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,42 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MainWrapper } from './Main.style';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            width: window.innerWidth
-        };
-    }
 
-    componentDidMount() {
-        window.addEventListener('resize', this.handleResize);
-        document.title = this.getTitle();
-    }
+const Main = () => {
+    const [width, setWidth] = useState(window.innerWidth)
+        useEffect(() => {
+            const handleResize = () => setWidth(window.innerWidth);
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
 
-    componentDidUpdate(_, prevState) {
-        if (prevState.width !== this.state.width) {
-            document.title = this.getTitle();
-        }
-    }
+        useEffect(() => {
+            const title = width < 300 ? "It's too small!" : "It's ok";
+            document.title = title;
+            return () => (document.title = '');
+        }, [width]);
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize);
-        document.title = '';
-    }
-
-    handleResize = () => this.setState({ width: window.innerWidth });
-
-    getTitle = () => (this.state.width < 300 ? "It's too small!" : "It's ok");
-
-    render() {
-        return (
-            <MainWrapper>
-                <h2>EX5_useEffect_intermediate</h2>
-                <p>{`the width is: ${this.state.width}`}</p>
-            </MainWrapper>
-        );
-    }
-}
+    return (
+        <MainWrapper>
+            <h2>EX5_useEffect_intermediate</h2>
+            <p>{`the width is: ${width}`}</p>
+        </MainWrapper>
+    );
+};
 
 export default Main;
